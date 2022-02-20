b528f1383b93c88ca0c7edd907746cb3448c08bc
Youssef <youssef.b.air2@gmail.com>
2022-02-20 21:46:18 +0100

    Initial Commit

---
+++ Skate App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skate App
+//
+//  Created by Youssef Bhl on 20/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
